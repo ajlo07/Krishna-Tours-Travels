6ae564d81d46eb9448f6c289a5979c6677357145
Aditya <ajaykumarkhaniya663@gmail.com>
2023-05-23 22:25:46 +0530

    remove section

--- index.php
+++ index.php
@@ -317,60 +317,7 @@
 
 
 
-<section class="trending-podcast-section section-padding">
-    <div class="container">
-        <div class="row">
 
-            <div class="col-lg-12 col-12">
-                <div class="section-title-wrap mb-5">
-                    <h4 class="section-title">Trending </h4>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-12 mb-4 mb-lg-0">
-                <div class="custom-block custom-block-full">
-                    <div class="custom-block-image-wrap">
-                        <a href="detail-page.html">
-                            <img src="images/podcast/27376480_7326766.jpg" class="custom-block-image img-fluid" alt="">
-                        </a>
-                    </div>
-
-                    <div class="custom-block-info">
-                        <h5 class="mb-2">
-                            <a href="detail-page.html">
-                                Vintage Show
-                            </a>
-                        </h5>
-
-                        <p class="mb-0">Lorem Ipsum dolor sit amet
-                            consectetur</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-12 mb-4 mb-lg-0">
-                <div class="custom-block custom-block-full">
-                    <div class="custom-block-image-wrap">
-                        <a href="detail-page.html">
-                            <img src="images/podcast/27376480_7326766.jpg" class="custom-block-image img-fluid" alt="">
-                        </a>
-                    </div>
-
-                    <div class="custom-block-info">
-                        <h5 class="mb-2">
-                            <a href="detail-page.html">
-                                Vintage Show
-                            </a>
-                        </h5>
-
-                        <p class="mb-0">Lorem Ipsum dolor sit amet
-                            consectetur</p>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </div>
-</section>
 </main>
 
 <?php include "footer.php" ?>
