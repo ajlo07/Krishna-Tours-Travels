73a980d25d97a765b4c003ef1d0d3fa681d5a654
ajlo07 <ajaykumarkhaniya663@gmail.com>
2023-05-25 22:16:16 +0530

    image changes

--- blog.php
+++ blog.php
@@ -23,7 +23,7 @@
                 </div>
             </div>
             
-            <div class="col-lg-4 col-12 mb-4 mb-lg-0">
+            <div class="col-lg-12 col-12 mb-4 mb-lg-0">
                 <div class="custom-block custom-block-full">
                     <div class="custom-block-image-wrap">
                         <a href="detail-page.html">
@@ -39,8 +39,7 @@
                             at an altitude of 3,583 meters in the majestic Himalayas, it holds immense religious
                             significance for Hindus. The town is home to the ancient Kedarnath Temple, dedicated to Lord
                             Shiva, believed to have been built by the Pandavas from the Mahabharata. This sacred temple
-                            is one of the twelve Jyotirlingas, symbolizing Lord Shiva's cosmic light.<span
-                                id="dots">...</span><span id="more">
+                            is one of the twelve Jyotirlingas, symbolizing Lord Shiva's cosmic light.
 
                                 To reach Kedarnath, pilgrims embark on a challenging 16-kilometer trek from Gaurikund,
                                 passing through picturesque landscapes, meadows, and valleys. The temple's grandeur,
@@ -52,14 +51,46 @@
                                 Kedarnath attracts millions of devotees and tourists annually, who seek blessings and a
                                 connection with the divine. The pilgrimage season begins in April or May, continuing
                                 until October or November. During this time, visitors offer prayers and immerse
-                                themselves in the profound spirituality of the temple.</span></p>
-                        <button onclick="myFunction()" id="myBtn">Read more</button>
+                                themselves in the profound spirituality of the temple.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-12 col-12 mb-4 mb-lg-0">
+                <div class="custom-block custom-block-full">
+                    <div class="custom-block-image-wrap">
+                        <a href="detail-page.html">
+                            <img src="images/cities/kedarnath.png" class="custom-block-image img-fluid" alt="">
+                        </a>
+                    </div>
+
+                    <div class="custom-block-info">
+                        <h5 class="mb-2">
+                            Kedarnath
+                        </h5>
+                        <p class="mb-0">Kedarnath is a revered pilgrimage site situated in Uttarakhand, India. Nestled
+                            at an altitude of 3,583 meters in the majestic Himalayas, it holds immense religious
+                            significance for Hindus. The town is home to the ancient Kedarnath Temple, dedicated to Lord
+                            Shiva, believed to have been built by the Pandavas from the Mahabharata. This sacred temple
+                            is one of the twelve Jyotirlingas, symbolizing Lord Shiva's cosmic light.
+
+                                To reach Kedarnath, pilgrims embark on a challenging 16-kilometer trek from Gaurikund,
+                                passing through picturesque landscapes, meadows, and valleys. The temple's grandeur,
+                                constructed with large stone slabs, leaves visitors in awe of its spiritual ambiance.
+                                Surrounding Kedarnath are breathtaking snow-capped peaks, including the majestic
+                                Kedarnath Peak. The holy Mandakini River flows near the temple, enhancing the serenity
+                                of the area.
+
+                                Kedarnath attracts millions of devotees and tourists annually, who seek blessings and a
+                                connection with the divine. The pilgrimage season begins in April or May, continuing
+                                until October or November. During this time, visitors offer prayers and immerse
+                                themselves in the profound spirituality of the temple.</p>
 
                     </div>
                 </div>
             </div>
 
-            <div class="col-lg-4 col-12 mb-4 mb-lg-0">
+            <div class="col-lg-12 col-12 mb-4 mb-lg-0">
                 <div class="custom-block custom-block-full">
                     <div class="custom-block-image-wrap">
                         <a href="detail-page.html">
@@ -75,8 +106,7 @@
                             at an altitude of 3,583 meters in the majestic Himalayas, it holds immense religious
                             significance for Hindus. The town is home to the ancient Kedarnath Temple, dedicated to Lord
                             Shiva, believed to have been built by the Pandavas from the Mahabharata. This sacred temple
-                            is one of the twelve Jyotirlingas, symbolizing Lord Shiva's cosmic light.<span
-                                id="dots">...</span><span id="more">
+                            is one of the twelve Jyotirlingas, symbolizing Lord Shiva's cosmic light.
 
                                 To reach Kedarnath, pilgrims embark on a challenging 16-kilometer trek from Gaurikund,
                                 passing through picturesque landscapes, meadows, and valleys. The temple's grandeur,
@@ -88,45 +118,7 @@
                                 Kedarnath attracts millions of devotees and tourists annually, who seek blessings and a
                                 connection with the divine. The pilgrimage season begins in April or May, continuing
                                 until October or November. During this time, visitors offer prayers and immerse
-                                themselves in the profound spirituality of the temple.</span></p>
-                        <button onclick="myFunction()" id="myBtn">Read more</button>
-
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-12 mb-4 mb-lg-0">
-                <div class="custom-block custom-block-full">
-                    <div class="custom-block-image-wrap">
-                        <a href="detail-page.html">
-                            <img src="images/cities/kedarnath.png" class="custom-block-image img-fluid" alt="">
-                        </a>
-                    </div>
-
-                    <div class="custom-block-info">
-                        <h5 class="mb-2">
-                            Kedarnath
-                        </h5>
-                        <p class="mb-0">Kedarnath is a revered pilgrimage site situated in Uttarakhand, India. Nestled
-                            at an altitude of 3,583 meters in the majestic Himalayas, it holds immense religious
-                            significance for Hindus. The town is home to the ancient Kedarnath Temple, dedicated to Lord
-                            Shiva, believed to have been built by the Pandavas from the Mahabharata. This sacred temple
-                            is one of the twelve Jyotirlingas, symbolizing Lord Shiva's cosmic light.<span
-                                id="dots">...</span><span id="more">
-
-                                To reach Kedarnath, pilgrims embark on a challenging 16-kilometer trek from Gaurikund,
-                                passing through picturesque landscapes, meadows, and valleys. The temple's grandeur,
-                                constructed with large stone slabs, leaves visitors in awe of its spiritual ambiance.
-                                Surrounding Kedarnath are breathtaking snow-capped peaks, including the majestic
-                                Kedarnath Peak. The holy Mandakini River flows near the temple, enhancing the serenity
-                                of the area.
-
-                                Kedarnath attracts millions of devotees and tourists annually, who seek blessings and a
-                                connection with the divine. The pilgrimage season begins in April or May, continuing
-                                until October or November. During this time, visitors offer prayers and immerse
-                                themselves in the profound spirituality of the temple.</span></p>
-                        <button onclick="myFunction()" id="myBtn">Read more</button>
-
+                                themselves in the profound spirituality of the temple.</p>
                     </div>
                 </div>
             </div>
