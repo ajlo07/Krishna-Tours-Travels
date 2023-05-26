3354b223d0213ae4630b0c78dcf36de45a32df1a
Aditya <ajaykumarkhaniya663@gmail.com>
2023-05-26 22:41:51 +0530

    Content changes

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12 col-12 text-center">
-                <h2 class="mb-0">Top Tours And Travels Agency In Rajkot</h2>
+                <h2 class="mb-0">Top Cab Service In Rajkot</h2>
             </div>
 
         </div>
@@ -19,14 +19,14 @@
                 <div class="pb-5 mb-5">
                     <div class="section-title-wrap mb-4">
                         <h4 class="section-title">Our story</h4>
-                    </div>
-                    <p>Welcome to Krushna Tours and Travels, the top tours and travels agency in Rajkot! With a passion for wanderlust and a commitment to exceptional service, we specialize in creating personalized and immersive journeys that cater to all your travel desires. Whether you're in search of thrilling adventures, relaxing retreats, or cultural explorations, we have the perfect options for you.</p>
+                        <img src="images/profile/about banner.png" class="about-image mt-5 img-fluid"
+                        alt="cab service in rajkot">
+                    <p>Welcome to Krushna Cab Service, the top cab service in Rajkot! With a passion for wanderlust and a commitment to exceptional service, we specialize in creating personalized and immersive journeys that cater to all your travel desires. Whether you're in search of thrilling adventures, relaxing retreats, or cultural explorations, we have the perfect options for you.</p>
                     <p>As the leading tours and travels agency in Rajkot, we understand that every traveler is unique. That's why we offer a diverse range of travel options to suit all preferences. From family vacations to romantic getaways, solo adventures to group tours, our bespoke itineraries are tailored to your interests, budget, and schedule. Our experienced travel experts will be with you every step of the way, ensuring that your journey is seamless and memorable.</p>
                     <p>Our travel packages extend beyond Rajkot, encompassing captivating destinations throughout India and across the globe. Whether you dream of exploring the pristine beaches of Goa, embarking on a spiritual journey in Varanasi, or witnessing the majestic Taj Mahal in Agra, our carefully crafted itineraries showcase the best of each destination. Leave all the details to us, from flights and accommodations to guided tours and local experiences, so you can focus on creating lasting memories.</p>
                     <p>At Krushna Tours and Travels, we believe that travel is not just about visiting new places, but also about creating connections and fostering cultural understanding. We are committed to promoting responsible and sustainable tourism practices, respecting the environment, and supporting local communities. Our collaborations with trusted partners who share our values ensure that your travel experience leaves a positive impact on the destinations you visit.</p>
-                    <p>Choose Krushna Tours and Travels as your trusted companion for extraordinary travel experiences in Rajkot and beyond. Contact us today to start planning your next adventure and let us make your travel dreams come true. Get ready to embark on a remarkable journey with the top tours and travels agency in Rajkot</p>
-                    <img src="images/profile/Car/aura.jpg" class="about-image mt-5 img-fluid"
-                        alt="">
+                    <p>Choose Krushna Tours and Travels as your trusted companion for extraordinary travel experiences in Rajkot and beyond. Contact us today to start planning your next adventure and let us make your travel dreams come true. Get ready to embark on a remarkable journey with the top cab service in Rajkot</p>
+                    </div>
                 </div>
             </div>
 
